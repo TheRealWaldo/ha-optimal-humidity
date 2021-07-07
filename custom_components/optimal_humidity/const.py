b52ebe94a45de47edb0d94cf828dd2a033384ea4
Will Freeman <6641284+TheRealWaldo@users.noreply.github.com>
2021-07-07 15:54:22 +0000

    fix!: correct term is specific humidity
    It was discovered through preparing to document this integration,
    that the unit of measurement we are using is actually Specific Humidity
    rather than Absolute Humidity.
    
    This updates all references accordingly.
    
    BREAKING CHANGE: absolute_humidity has become specific_humidity

--- custom_components/optimal_humidity/const.py
+++ custom_components/optimal_humidity/const.py
@@ -17,7 +17,7 @@ VERSION = "0.1.0"
 ISSUE_URL = "https://github.com/TheRealWaldo/ha-optimal-humidity/issues"
 
 ATTR_DEWPOINT = "dewpoint"
-ATTR_ABSOLUTE_HUMIDITY = "absolute_humidity"
+ATTR_SPECIFIC_HUMIDITY = "specific_humidity"
 ATTR_OPTIMAL_HUMIDITY = "optimal_humidity"
 ATTR_CRITICAL_HUMIDITY = "critical_humidity"
 ATTR_MOLD_WARNING = "mold_warning"
@@ -26,9 +26,9 @@ CONF_CRITICAL_TEMP = "critical_temp_sensor"
 CONF_INDOOR_HUMIDITY = "indoor_humidity_sensor"
 CONF_INDOOR_TEMP = "indoor_temp_sensor"
 CONF_INDOOR_PRESSURE = "indoor_pressure_sensor"
-CONF_OPTIMAL_ABSOLUTE_HUMIDITY = "optimal_absolute_humidity"
+CONF_OPTIMAL_SPECIFIC_HUMIDITY = "optimal_specific_humidity"
 # 7g/m³ seems to be a comfortable default
-DEFAULT_OPTIMAL_ABSOLUTE_HUMIDITY = 7
+DEFAULT_OPTIMAL_SPECIFIC_HUMIDITY = 7
 
 DEFAULT_NAME = "Optimal Humidity"
 
@@ -39,8 +39,8 @@ SENSOR_TYPES = {
         DEVICE_CLASS_TEMPERATURE,
         "hass:thermometer",
     ),
-    ATTR_ABSOLUTE_HUMIDITY: (
-        "absolute_humidity",
+    ATTR_SPECIFIC_HUMIDITY: (
+        "specific_humidity",
         "g_H₂O g_Air⁻¹",
         "",
         "mdi:water",
